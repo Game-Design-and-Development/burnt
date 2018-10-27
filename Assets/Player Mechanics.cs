80a351672d173066356b60bd2cf8b78ee3d266f8
Shawnshark <frahmans@uwm.edu>
2018-10-26 23:23:01 -0500

    Added basic mechanic variables that are currently not in use

---
+++ Assets/Player Mechanics.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMechanics : MonoBehaviour {
+
+    private bool pit = false; // If player is over pit, pit = true
+    private bool fire = false; // If player is hit by fire, fire = true
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
