b98e133b0e202870126420f55947c4adcf7033d5
rpthorne <rpthorne@uwm.edu>
2018-11-06 15:23:15 -0600

    added intro audio to LevelManager, should probably be in GameManager?

---
+++ Assets/Scripts/s_LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+ * @author Ryan Thorne,
+ * levelmanager controls the state of the current level. this can be a parent class to the individual levels :>
+ * 
+ *
+ */
+
+
+public class s_LevelManager : MonoBehaviour {
+
+	public AudioSource audio_source;
+	// Use this for initialization
+	void Start () {
+		audio_source = GetComponent<AudioSource>();
+		audio_source.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
