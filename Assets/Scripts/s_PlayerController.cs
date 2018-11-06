31ef936b3d9d3a5605146837465e646276b84bdc
rpthorne <rpthorne@uwm.edu>
2018-11-06 13:46:10 -0600

    added extra org folders, more code for collisions

--- Assets/Scripts/s_PlayerController.cs
+++ Assets/Scripts/s_PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using System.Math;
 
 /**
  * player controller
@@ -10,25 +11,62 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour {
 
+	//speed/directional values
 	Vector3 IN;
-	public int MAX_DIRECTIONAL_SPEED; // all caps should be readonly, unsure if i can get away with it at this point :)
+	public uint MAX_DIRECTIONAL_SPEED; // all caps should be readonly, unsure if i can get away with it at this point :)
 	public float DIRECTIONAL_DEADZONE;
+	protected float zero_speed = 0.0f;
+	
+	
+	//collision data
+	protected ContactFilter2D l_enemy;
+	protected ContactFilter2D l_wall;
+	protected ContactFilter2D l_goal;
+	protected ContactFilter2D l_hazard;
+	protected RaycastHit2D[] hitbuffer;
 	protected Rigidbody2D collision_detector = null;
 
 	//using onEnable in case object is not created as sscene starts?
 	void OnEnable()
     {
         collision_detector = GetComponent<Rigidbody2D> ();
+		hitbuffer = new RaycastHit2D[16];
     }
 	
 	// Update is called once per frame
 	void Update () {
 		read_input();
 		//refine movement
-		IN *= Time.deltaTime * MAX_DIRECTIONAL_SPEED;
+		uint speed = Time.deltaTime * MAX_DIRECTIONAL_SPEED;
+		IN *= speed;
 		
 		//resolve collisions
 		
+		int hits;
+		//3 collisions: with enemies/hazards (dynamic), with walls, (cancel move), with goals (triggering)
+		
+		if(hits = collision_detector.Cast(IN , l_wall, hitbuffer, speed) > 0)
+		//collision with wall means we can cozy up to it and then cancel movement parrallel to the collision direction
+		{
+			for(int i = 0; i < hits; i++)
+			{
+				//find the length of the compnent of the vector that is parrallel to the norm of the collision: subtract that from our movement vector
+				IN -= hitbuffer[i].normal * speed * hitbuffer[i].fraction;
+			}
+		}
+		
+		if(hits = collision_detector.Cast(IN , l_enemy, hitbuffer, speed) > 0)
+		//resolve enemy collision results on movement
+		{
+			
+		}
+		
+		if(hits = collision_detector.Cast (IN, l_goal, hitbuffer, speed) > 0)
+		{
+			
+		}
+		
+		//result 
 		
 		//actually translate object
 		
@@ -38,10 +76,10 @@ public class playerController : MonoBehaviour {
 	void read_input()
 	{
 		IN.x = Input.GetAxis("horizontal");
-		if(System.Math.Abs(IN.x) < DIRECTIONAL_DEADZONE)
+		if(Mathf.Abs(IN.x) < DIRECTIONAL_DEADZONE)
 			IN.x = zero_speed;
 		IN.y = Input.GetAxis("vertical");
-		if(System.Math.Abs(IN.y) < DIRECTIONAL_DEADZONE)
+		if(Mathf.Abs(IN.y) < DIRECTIONAL_DEADZONE)
 			IN.y = zero_speed;
 	}
 }
