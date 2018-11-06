674ed8ab5d4bfbb7b44ec6c42fe88bd704c03f9e
Shawnshark <frahmans@uwm.edu>
2018-11-05 20:42:56 -0600

    Stuff

---
+++ Assets/Scripts/Player Mechanics.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMechanics : MonoBehaviour {
+
+    private bool hazard = false; // If player is over hazard, hazard = true
+    private bool wizard; //Checks to see if player is wizard or warrior. Wizard = true, Warrior = false
+
+    public Vector3 push; // A Vector 3 telling the offset to attack
+    public Object fire;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    // Will be called when player attacks
+    public void attack()
+    {
+        Instantiate(fire,transform.position + push, Quaternion.identity); // Fire is placeholder for either magic attack or sword depending on class
+    }
+}
